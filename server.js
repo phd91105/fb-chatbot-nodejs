732e82aed1acaaad061d3c971c84890f516cc1b0
Phạm Duy <PHD051199@gmail.com>
2020-12-04 22:31:22 +0700

    Update server.js

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ app.post("/webhook", function (req, res) {
                         if (i < 7) {
                           myLoop();
                         }
-                      }, 1000);
+                      }, 2000);
                     }
                     myLoop();
                     // }
@@ -235,7 +235,7 @@ app.post("/webhook", function (req, res) {
                         if (i < 7) {
                           myLoop();
                         }
-                      }, 1000);
+                      }, 2000);
                     }
                     myLoop();
                     // }
